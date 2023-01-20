a55d40f531aea84eff42bbe79ba2a76f7e4842e3
Hendrik Gossler <h.gossler@omegadot.software>
2023-01-20 15:27:33 +0100

    Add commonjs outputs to assert and fs package

---
+++ build.ts
@@ -0,0 +1,21 @@
+import {build, BuildOptions} from "esbuild";
+
+
+for(const pkg of ["assert", "fs"]) {
+    buildCJS(`packages/${pkg}`)
+}
+
+function buildCJS(packagePath: string) {
+    const options: BuildOptions = {
+        entryPoints: [`${packagePath}/dist/src/index.js`],
+        bundle: true,
+        sourcemap: true,
+        platform: "node",
+        outdir: `${packagePath}/dist/cjs`,
+    };
+
+    build(options).catch(() => {
+        process.exit(1);
+    });
+}
+
