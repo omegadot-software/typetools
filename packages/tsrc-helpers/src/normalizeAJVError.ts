9ccc3cc0761a4811030ff83855fa190770e9dbce
Hendrik Gossler <h.gossler@omegadot.software>
2023-02-01 14:58:54 +0100

    Upgrade dev tools, configure prettier standalone

--- packages/tsrc-helpers/src/normalizeAJVError.ts
+++ packages/tsrc-helpers/src/normalizeAJVError.ts
@@ -2,12 +2,12 @@ import { ErrorObject } from "ajv";
 
 export function normalizeAJVError(e: ErrorObject) {
 	const params = Object.keys(e.params)
-		.map((param) => `${param}: ${(e.params as any)[param] as string}`)
+		.map((param) => `${param}: ${(e.params as never)[param] as string}`)
 		.join(", ");
 
 	return new Error(
 		`Configuration ${e.dataPath.length ? `field ${e.dataPath.slice(1)}` : ""} ${
-			e.message
+			e.message ?? ""
 		} (${params})`
 	);
 }
